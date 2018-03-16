d4da6038c45fda3d7544f6dc0f9ac84f9995506a
Chad Huntley <chad.huntley@toyota.com>
2018-03-16 09:20:31 -0700

    Updated to Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,12 @@
 const webpack = require('webpack');
 const path = require('path');
 
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const plugins = [
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.optimize.ModuleConcatenationPlugin(),
-  new webpack.NamedModulesPlugin(),
-  new HtmlWebpackPlugin({
-    hash: true,
-    template: 'index.template.html',
-  }),
-  new CleanWebpackPlugin(['dist'], {
-    verbose: true,
-    dry: false,
-  }),
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: (module) => {
-      return module.context && module.context.indexOf('node_modules') !== -1;
-    },
-  }),
-  new webpack.optimize.CommonsChunkPlugin({
-    name: 'manifest',
-  }),
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-    }
-  }),
-  new webpack.SourceMapDevToolPlugin({
-    filename: '[name].js.map',
-    exclude: ['vendor']
-  }),
-];
-
-if (isProduction) {
-  // needs to be loaded before sourcemap plugin
-  plugins.splice(-1, 0, new webpack.optimize.UglifyJsPlugin({
-    sourceMap: true,
-    compress: {
-      warnings: false
-    }
-  }))
-}
-
 module.exports = {
+  mode: process.env.NODE_ENV,
   entry: {
     main: './src/app.js',
-  },
-  output: {
-    path: `${__dirname}/dist`,
-    chunkFilename: '[name].[chunkhash].[id].js',
-    filename: '[name].[hash].js',
-    publicPath: '/',
   },
   module: {
     rules: [
@@ -69,12 +19,10 @@ module.exports = {
       },
     ],
   },
-  plugins,
-  stats: { colors: true },
-  devServer: {
-    hot: true,
-    port: 8090,
-    publicPath: '/',
-    historyApiFallback: { index: '/' },
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: 'index.template.html',
+    }),
+  ],
 };
