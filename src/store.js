e397b752bec9b70805a13f6cc80ea24dbb489f7b
Chad Huntley <chad.huntley@toyota.com>
2018-08-09 14:30:10 -0700

    Added redux reducer hot module reloading

--- src/store.js
+++ src/store.js
@@ -7,10 +7,16 @@ import sagas from './redux/sagas';
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
   applyMiddleware(sagaMiddleware),
 );
 
 sagaMiddleware.run(sagas);
+
+if (module.hot) {
+  module.hot.accept('./redux/reducers', () => {
+    store.replaceReducer(require('./redux/reducers').default); // eslint-disable-line global-require
+  });
+}
 
 export default store;
