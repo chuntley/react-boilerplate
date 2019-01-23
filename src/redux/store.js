d571d70a1e477ab8805bf6a275abf31b988ccfdc
Chad Huntley <chadhuntley@gmail.com>
2019-01-22 16:41:03 -0800

    Upgrade all dependencies and fixes for compatibility

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 
 import reducers from './reducers';
 import sagas from './sagas';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   reducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(sagaMiddleware),
+  composeEnhancer(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(sagas);
