84b11000c419864ba1b4dae2524c81a51806c099
Chad Huntley <chuntley@indeed.com>
2021-11-01 15:59:04 -0700

    Fixed "Home" test

--- src/pages/__tests__/Home.test.tsx
+++ src/pages/__tests__/Home.test.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { render } from "utils/rtl-wrapper";
+import { renderWithRouter } from "utils/rtl-wrapper";
 
 import Home from "../Home";
 
 it("renders <Home /> page", () => {
-  const { getByText } = render(<Home />);
+  const { getByText } = renderWithRouter(<Home />);
   expect(getByText("Welcome!")).toBeTruthy();
 });
