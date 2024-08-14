7f0cc1061c38b367990f4d7f68615dfe8a53a551
Chad Huntley <chuntley@indeed.com>
2024-08-14 15:32:04 -0700

    Use react-error-boundary package instead of custom class

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ErrorBoundary } from "react-error-boundary";
 
 import App from "@/components/App";
-import ErrorBoundary from "@/components/ErrorBoundary";
 
 const container = document.getElementById("app");
 if (!container) throw new Error("Failed to find the root element");
 const root = createRoot(container);
 
 root.render(
-  <ErrorBoundary>
+  <ErrorBoundary fallback={<div>Something went wrong</div>}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
