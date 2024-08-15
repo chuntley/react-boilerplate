6098aaf3449618c2f97bd17e1a6c1a7bbbdf9107
Chad Huntley <chuntley@indeed.com>
2024-08-15 08:41:20 -0700

    Removed error boundary example

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ErrorBoundary } from "react-error-boundary";
 
 import App from "@/components/App";
 
@@ -10,9 +9,7 @@ if (!container) throw new Error("Failed to find the root element");
 const root = createRoot(container);
 
 root.render(
-  <ErrorBoundary fallback={<div>Something went wrong</div>}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ErrorBoundary>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,12 +6,3 @@ interface StoreState {
 interface LayoutProps {
   children: JSX.Element;
 }
-
-interface ErrorBoundaryProps {
-  children: JSX.Element;
-}
-
-interface ErrorBoundaryState {
-  error: Error | null;
-  errorInfo: React.ErrorInfo | null;
-}
