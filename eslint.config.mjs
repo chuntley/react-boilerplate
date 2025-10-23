173bbf755afa20f5cf7be71ad28e654f5a6c83ed
Chad Huntley <chuntley@indeed.com>
2025-10-23 09:11:59 -0700

    Integrated React Compiler

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
@@ -8,6 +9,14 @@ export default tseslint.config(
   react.configs.flat.recommended,
   prettierRecommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "react-hooks": reactHooks,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+    },
+  },
   {
     settings: {
       react: {
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,14 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react({ jsxRuntime: "automatic" })],
+  plugins: [
+    react({
+      jsxRuntime: "automatic",
+      babel: {
+        plugins: ["babel-plugin-react-compiler"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
