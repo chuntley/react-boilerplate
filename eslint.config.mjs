37721315a422902bf31288eb93e0363e4a8e8a7c
Chad Huntley <chuntley@indeed.com>
2025-11-26 12:02:48 -0800

    Fixed eslint deprecation notice

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,11 @@
 import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   react.configs.flat.recommended,
   prettierRecommended,
