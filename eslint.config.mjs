b05ee78d4f149b5c114b91e50324701ba83cf823
Chad Huntley <chadhuntley@gmail.com>
2026-03-06 12:20:19 -0800

    Updated packages, simplified eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,15 @@
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
+import eslintReact from "@eslint-react/eslint-plugin";
 import prettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default defineConfig(
   eslint.configs.recommended,
-  react.configs.flat.recommended,
-  prettierRecommended,
   ...tseslint.configs.recommended,
   {
-    plugins: {
-      "react-hooks": reactHooks,
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off",
-    },
+    files: ["**/*.{ts,tsx}"],
+    ...eslintReact.configs["recommended-typescript"],
   },
+  prettierRecommended,
 );
